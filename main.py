f2062fd3ce927ce8c0f4ce2c5b9872f8f5021553
Muhamad Fachrul (EI) <fachrulm@n82111.nbi.ac.uk>
2017-07-25 16:36:58 +0100

    First commit!

--- main.py
+++ main.py
@@ -16,26 +16,35 @@ def bestone(row):
     """Function to create rank column"""
     # Between the triple " is a doctstring, i.e. the documentation for the funtion
 
-    if row['ccode'] == '=':
+    # assert isinstance(row, pd.DataFrame), (row, row.keys())
+
+    # val = 1
+    if row["CCODE"] is np.nan:
+        val = 1
+    elif row["CCODE"] == '=':
         val = 10
-    elif row['ccode'] == '_':
+    elif row["CCODE"] == '_':
         val = 9
-    elif 'f' and '=' in row['ccode']:
-        val = 8
-    elif 'f' and '_' in row['ccode']:
-        val = 7
-    elif row['ccode'] in ('n', 'J', 'C', 'c'):
+    elif 'f' in row["CCODE"]:
+        if "=" in row["CCODE"]:
+            val = 8
+        elif '_' in row["CCODE"]:
+            val = 7
+        else:
+            val = 4
+    elif row["CCODE"] in ('n', 'J', 'C', 'c'):
         val = 6
-    elif row['ccode'] in ('j', 'h', 'g', 'G'):
+    elif row["CCODE"] in ('j', 'h', 'g', 'G'):
         val = 5
-    elif row['ccode'] in ('o', 'e', 'm'):
+    elif row["CCODE"] in ('o', 'e', 'm'):
         val = 4
-    elif row['ccode'] in ('i', 'I', 'ri', 'rI'):
+    elif row["CCODE"] in ('i', 'I', 'ri', 'rI'):
         val = 3
-    elif row['ccode'] in ('x', 'X', 'p', 'P'):
+    elif row["CCODE"] in ('x', 'X', 'p', 'P'):
         val = 2
     else:
         val = 1
+
     return val
 
 
@@ -73,48 +82,66 @@ def load_ref_stats(path):
     ref = pd.read_csv(path, sep='\t')
     ref = ref.loc[:, ['TID', '# coding exons']]
     ref.columns = ['TID_x', '# coding exons_x']
+
+    #print(ref.columns)
     return ref
+
 
 def load_aligned_stats(path):
     ali = pd.read_csv(path, sep='\t')
     ali = ali.loc[:, ['TID', "GID", 'Exon number']]
     strip_path(ali)
     ali.columns = ['TID_y', "GID_y", '# Exon number_y']
+
+    #print(ali.columns)
     return ali
 
 
 def load_comparisons(path):
     """Input is refmap file"""
-    refmap = dict()
+    # refmap = dict()
     com = pd.read_csv(path, sep='\t')
     com = com.loc[:, ['tid', "gid", 'ccode', "ref_id", 'ref_gene']]
     com.columns = com.columns.str.upper()
+
     strip_path(com)
-    a = (com['REF_ID'].str.contains('LC'))
-    refmap['LC'] = com.loc[a]
-    refmap['HC'] = com.loc[~a]
-    for i in ['LC','HC']:
-        refmap[i] = refmap[i].dropna()
-        refmap[i]['rank'] = refmap[i].apply(bestone, axis=1)
-        refmap[i]['category'] = refmap[i].apply(categ, axis=1)
-    return refmap
+    a = ~(com['REF_ID'].str.contains('LC'))
+    com["CONFIDENCE"] = a
+    # refmap['LC'] = com.loc[a]
+    # refmap['HC'] = com.loc[~a]
+    com.dropna()
+    com["rank"] = com.apply(bestone, axis=1)
+    com["category"] = com.apply(categ, axis=1)
+
+    # for i in ['LC','HC']:
+    #     refmap[i] = refmap[i].dropna()
+    #     refmap[i]['rank'] = refmap[i].apply()
+    #     refmap[i]['category'] = refmap[i]
+
+    #print(com.columns)
+    return com
 
 def init_merge(ref, aligned, comparison):
     merge = pd.merge(pd.merge(ref,aligned,left_on='TID_x',right_on='TID_y'),
                      comparison,left_on='TID_x',right_on='TID')
+    with open("merge.tsv", "wt") as out:
+        merge.to_csv(out, sep="\t")
+    #print(merge)
     return merge
 
 def pre_ref_merge(merge, x, z):
-    merge = merge[["TID_y", 'GID', "REF_ID", 'REF_GENE', "# coding exons_x", 'CCODE']]
-    merge.columns = ["{} id".format(x),
+    new_df = merge[["TID_y", 'GID', "REF_ID", 'REF_GENE', "# coding exons_x", 'CCODE']]
+    new_df.columns = ["{} id".format(x),
                      '{} gene'.format(x),
                      "ref ({}) id".format(z),
                      "ref ({}) gene".format(z),
                      "{} Exon(s)".format(x),
                      '{}-{} ccode'.format(x, z)]
-    merge.replace("-", np.nan, inplace=True)
-    merge.dropna(inplace=True)
-    return merge
+    new_df = new_df.replace("-", np.nan)
+    new_df = new_df.dropna()
+
+    #print(new_df)
+    return new_df
 
 def ref_merge(mergexz, mergeyz, x, y, z):
     mergexz = pre_ref_merge(mergexz, x, z)
@@ -130,28 +157,52 @@ def ref_merge(mergexz, mergeyz, x, y, z):
                      '{}-{} ccode'.format(y, z),
                      "ref ({}) id".format(z),
                      "ref ({}) gene".format(z)]]
+    with open("zmerge.tsv", "wt") as out:
+        zmerge.to_csv(out, sep="\t")
+    #print(zmerge)
     return zmerge
 
 
 def sixway(xy_z_merge, xz_y_merge, yz_x_merge, x, y, z):
 
-    pretrip = pd.merge(xy_z_merge, xz_y_merge,
-                       left_on=['{} id'.format(x), 'ref ({}) id'.format(z), '{} id'.format(y)],
-                       right_on=["{} id".format(x), "{} id".format(z), "ref ({}) id".format(y)]
-                       )
-    pretrip = pretrip.drop_duplicates(subset='{} id'.format(x), keep=False)
-    triplets = pd.merge(pretrip,
+    """
+
+    :param xy_z_merge:
+    :type xy_z_merge: pd.DataFrame
+    :param xz_y_merge:
+    :type xz_y_merge: pd.DataFrame
+    :param yz_x_merge:
+    :type yz_x_merge: pd.DataFrame
+    :param x:
+    :param y:
+    :param z:
+    :return:
+    """
+
+    pre_trip = pd.merge(xy_z_merge, xz_y_merge,
+                        left_on=['{} id'.format(x), 'ref ({}) id'.format(z),
+                                 '{} id'.format(y)],
+                        right_on=["{} id".format(x), "{} id".format(z), "ref ({}) id".format(y)])
+
+    pre_trip = pre_trip.drop_duplicates(subset='{} id'.format(x), keep=False)
+
+    triplets = pd.merge(pre_trip,
                         yz_x_merge,
-                        left_on=['{} id'.format(z), '{} id'.format(y), '{} id'.format(x)],
-                        right_on=['{} id'.format(z), '{} id'.format(y), 'ref ({}) id'.format(x)])
+                        left_on=['{} id'.format(x), '{} id'.format(y), '{} id'.format(z)],
+                        right_on=['ref ({}) id'.format(x), '{} id'.format(y), '{} id'.format(z)])
     triplets = triplets[['{} id'.format(z), '{} id'.format(y), '{} id'.format(x),
                          '{} Exon(s)_x'.format(z), '{} Exon(s)_x'.format(y), '{} Exon(s)_x'.format(x),
                          '{}-{} ccode'.format(z, y), '{}-{} ccode'.format(z, x), '{}-{} ccode'.format(y, z),
                          '{}-{} ccode'.format(y, x), '{}-{} ccode'.format(x, z), '{}-{} ccode'.format(x, y)]]
 
-    triplets.columns = [z, y, x, '{} Exon(s)'.format(z), '{} Exon(s)'.format(y), '{} Exon(s)'.format(x),
-                        '{}-{} ccode'.format(z, y), '{}-{} ccode'.format(z, x), '{}-{} ccode'.format(y, z),
-                        '{}-{} ccode'.format(y, x), '{}-{} ccode'.format(x, z), '{}-{} ccode'.format(x, y)]
+    triplets.columns = [x, y, z, '{} Exon(s)'.format(x), '{} Exon(s)'.format(y), '{} Exon(s)'.format(z),
+                        '{}-{} ccode'.format(x, y), '{}-{} ccode'.format(x, z), '{}-{} ccode'.format(y, x),
+                        '{}-{} ccode'.format(y, z), '{}-{} ccode'.format(z, x), '{}-{} ccode'.format(z, y)]
+
+    with open("triplets.tsv", "wt") as out:
+        triplets.to_csv(out, sep="\t")
+
+    print(triplets)
     return triplets
 
 
@@ -183,12 +234,15 @@ def excode(xlist, x, y, z):
     return bothm_ex
 
 def allmatches(df,x,y,z):
-    x = (df['{} Exon(s)_x'.format(x)] == df['{} Exon(s)_x'.format(y)]) & \
-        (df['{} Exon(s)_x'.format(y)] == df['{} Exon(s)_x'.format(z)])
+    xx = (df['{} Exon(s)'.format(x)] == df['{} Exon(s)'.format(y)]) & \
+        (df['{} Exon(s)'.format(y)] == df['{} Exon(s)'.format(z)])
     a = df['{}-{} ccode'.format(x, y)].isin(['=', '_']) & df['{}-{} ccode'.format(x, z)].isin(['=', '_']) &\
         df['{}-{} ccode'.format(y, x)].isin(['=', '_']) & df['{}-{} ccode'.format(y, z)].isin(['=', '_']) & \
         df['{}-{} ccode'.format(z, x)].isin(['=', '_']) & df['{}-{} ccode'.format(z, y)].isin(['=', '_'])
-    triplets_eq = df[x & a]
+    triplets_eq = df[xx & a]
+
+    print(triplets_eq)
+
     return triplets_eq
 
 
@@ -201,8 +255,9 @@ def main():
     parser.add_argument("A", help="\"A\" genome name (eg. 1A)")
     parser.add_argument("B", help="\"B\" genome name (eg. 1B)")
     parser.add_argument("D", help="\"D\" genome name (eg. 1D)")
+    parser.add_argument("--chroms", help="Chromosome numbers of wheat; 1 to 7")
     parser.add_argument("--refmap",
-                        help="Template for the REFMAP files, eg ?_on_?.compare.tmap. It must contain two ?",
+                        help="Template for the REFMAP files, eg ?_on_?.compare.refmap. It must contain two ?",
                         default="?_on_?.compare.refmap")
     parser.add_argument("--ref",
                         help="Template for the reference statistics, eg chr?.reference.stats.tsv. It must contain one ?",
@@ -211,8 +266,12 @@ def main():
                         help="Template for the aligned statistics, eg ?_on_?.stats.tsv. It must contain two ?",
                         default="?_on_?.stats.tsv")
     parser.add_argument("--existl",
-                        help="Existing list to compare to, eg wheat.homeolog_groups.chr1.")
+                        help="Existing df to compare to, eg wheat.homeolog_groups.TRIADS.chr?. The ? is the chromosome number",
+                        default="wheat.homeolog_groups.TRIADS.chr?")
+    parser.add_argument("--triplets",
+                        help="Df of generated triplets.")
     parser.add_argument("--out", help="Output file")
+
     # parser.add_argument("--inmerge",
     #                     help="Initial merge of the reference statistics, aligned statistics, and TMAP files. It must contain two ?",
     #                     default="nt_??")
@@ -223,6 +282,9 @@ def main():
     aligned_stats = dict()
     ref_stats = dict()
     initial_merge = dict()
+    refr_merge = dict()
+    triplets = dict()
+
 
     genomes = [args.A, args.B, args.D]
     # Load the reference statistics into the dictionary
@@ -230,24 +292,33 @@ def main():
         ref_stats[genome] = load_ref_stats(re.sub("\?", genome, args.ref))
 
     template = re.sub("\?", "{}", args.aligned)
-    comp_template = re.sub("\?", "{}", args.tmap)
+    comp_template = re.sub("\?", "{}", args.refmap)
 
     for x, y in itertools.permutations(genomes, 2):
         aligned_stats[(x, y)] = load_aligned_stats(template.format(x, y))
+
+    for x, y in itertools.permutations(genomes, 2):
         comparisons[(x, y)] = load_comparisons(comp_template.format(x, y))
 
     for x, y in itertools.permutations(genomes, 2):
-        initial_merge[(x, y)] = init_merge(ref_stats[y], aligned_stats[(x, y)], comparisons[(x, y)])
+        initial_merge[(x, y)] = init_merge(ref_stats[x], aligned_stats[(x, y)], comparisons[(x, y)])
         pre_ref_merge(initial_merge[(x, y)], x, y)
 
     for x, y, z in itertools.permutations(genomes, 3):
-        ref_merge[(x, y)] = ref_merge(init_merge[(x, z)], init_merge[(y, z)], x, y, z)
+        refr_merge[(x, y)] = ref_merge(initial_merge[(x, z)], initial_merge[(y, z)], x, y, z)
+
+    for z, y, x in itertools.combinations(genomes, 3):
+        triplets = sixway(refr_merge[(x, y)], refr_merge[(x, z)], refr_merge[(y, z)], x, y, z)
 
     for x, y, z in itertools.combinations(genomes, 3):
-        triplets = sixway(ref_merge[(x, y)], ref_merge[(x, z)], ref_merge[(y, z)], x, y, z)
+        triplets_eq = allmatches(triplets, x, y, z)
 
 
+    #for x, y, z in genomes:
+    #    for n in list(range(1, 8)):
+    #        crosschecked = crossch(re.sub("\?", n, args.existl), triplets, x, y, z)
 
+    # for
 
 
 
--- tests.py
+++ tests.py
@@ -7,7 +7,7 @@ class BestOneTester(unittest.TestCase):
 
     def test_match(self):
 
-        row = {"ccode": "="}
+        row = {"CCODE": "="}
 
         self.assertEqual(bestone(row), 10)
 
@@ -24,7 +24,7 @@ class BestOneTester(unittest.TestCase):
                 ('x', 2)]
 
         for ccode, val in vals:
-            row = {"ccode": ccode}
+            row = {"CCODE": ccode}
             self.assertEqual(bestone(row), val)
 
     def test_wrong_input(self):
@@ -39,9 +39,14 @@ class BestOneTester(unittest.TestCase):
 
     def test_match2(self):
 
-        row = {"ccode": "f, _"}
+        row = {"CCODE": "f, _"}
 
         self.assertEqual(bestone(row), 7)
+
+    #def test_dummy(self):
+     #   with open("az.tsv", "r") as ax:
+      #      df = bestone(ax)
+       #     self.assertEqual(df[0], 10)
 
 class CategTester(unittest.TestCase):
 
@@ -86,6 +91,136 @@ class refloadTester(unittest.TestCase):
             df = load_ref_stats(a)
             self.assertEqual(list(df.columns), refcol)
 
+    def test_dummy_ref(self):
+        refcol = ['TID_x', '# coding exons_x']
+        with open("chr1A.reference.stats.tsv", "r") as testref:
+            df = load_ref_stats(testref)
+            self.assertEqual(list(df.columns), refcol)
+
+class statsloadTester (unittest.TestCase):
+
+    def test_name_change(self):
+        refcol = ['TID_y', 'GID_y', '# Exon number_y']
+        with open("1A_on_1B.stats.tsv", "r") as a:
+            df = load_aligned_stats(a)
+            self.assertEqual(list(df.columns), refcol)
+
+class comparloadTester (unittest.TestCase):
+
+    def test_name_change(self):
+        refcol = ['TID', 'GID', 'CCODE', 'REF_ID', 'REF_GENE', "CONFIDENCE", "rank", "category"]
+        with open("1D_on_1A.compare.refmap", "r") as a:
+            df = load_comparisons(a)
+            self.assertIsInstance(df, pd.DataFrame)
+            self.assertEqual(list(df.columns), refcol)
+
+
+class InitMergeTester(unittest.TestCase):
+
+    def test_init(self):
+        def init_merge(ref, aligned, comparison):
+            merge = pd.merge(pd.merge(ref, aligned, left_on='TID_x', right_on='TID_y'),
+                             comparison, left_on='TID_x', right_on='TID')
+            return merge
+        ref = pd.DataFrame(columns=['TID_x', '# coding exons_x'])
+        aligned = pd.DataFrame(columns=['TID_y', "GID_y", '# Exon number_y'])
+        compa = pd.DataFrame(columns=['TID', "GID", 'CCODE', "REF_ID", 'REF_GENE', 'rank', 'category'])
+        merge = init_merge(ref, aligned, compa)
+
+        cols = list(merge.columns)
+        self.assertEqual(cols,
+                         ['TID_x', '# coding exons_x', 'TID_y', 'GID_y', '# Exon number_y', 'TID', "GID", 'CCODE',
+                          "REF_ID", 'REF_GENE', 'rank', 'category'])
+
+
+
+
+    def test_one(self):
+
+        df = pd.DataFrame(columns=["TID_y", 'GID', "REF_ID", 'REF_GENE', "# coding exons_x", 'CCODE', "extra"])
+        df.loc[0] = ["tid_1", "gid_1", "ref_1", "ref_gid_1", 2, "=", "foo"]
+        df.loc[1] = ["tid_2", "gid_2", "-", "-", 2, "-", "bar"]
+        self.assertEqual(len(df), 2)
+        new_df = pre_ref_merge(df, "A", "B")
+        self.assertEqual(len(new_df), 1, new_df)
+        cols = list(new_df.columns)
+        self.assertEqual(cols,
+                         ["A id", "A gene", "ref (B) id", "ref (B) gene", "A Exon(s)", "A-B ccode"])
+        row = new_df[new_df["A id"] == "tid_2"]
+        self.assertEqual(len(row), 0, row)
+        row = new_df[new_df["A id"] == "tid_1"]
+        self.assertEqual(len(row), 1, new_df)
+
+    # def ref_merge(mergexz, mergeyz, x, y, z):
+    #     mergexz = pre_ref_merge(mergexz, x, z)
+    #     mergeyz = pre_ref_merge(mergeyz, y, z)
+    #     zmerge = pd.merge(mergexz, mergeyz, how='inner', on=["ref ({}) id".format(z), "ref ({}) gene".format(z)])
+    #     zmerge = zmerge[["{} id".format(x),
+    #                      "{} gene".format(x),
+    #                      "{} Exon(s)".format(x),
+    #                      '{}-{} ccode'.format(x, z),
+    #                      "{} id".format(y),
+    #                      "{} gene".format(y),
+    #                      "{} Exon(s)".format(y),
+    #                      '{}-{} ccode'.format(y, z),
+    #                      "ref ({}) id".format(z),
+    #                      "ref ({}) gene".format(z)]]
+    #     return zmerge
+
+    def test_ref_merge_one(self):
+        df = pd.DataFrame(columns=["TID_y", 'GID', "REF_ID", 'REF_GENE', "# coding exons_x", 'CCODE', "extra"])
+        df_one = pd.DataFrame(columns=["TID_y", 'GID', "REF_ID", 'REF_GENE', "# coding exons_x", 'CCODE', "extra"])
+
+        df.loc[0] = ["tidA_1", "gidA_1", "refB_1", "refB_gid_1", 2, "=", "foo"]
+        df.loc[1] = ["tidA_2", "gidA_2", "-", "-", 2, "-", "bar"]
+
+        df_one.loc[0] = ["tidD_1", "gidD_1", "refB_1", "refB_gid_1", 2, "=", "foo"]
+        df_one.loc[1] = ["tidD_2", "gidD_2", "-", "-", 2, "-", "bar"]
+
+        merged = ref_merge(df, df_one, "A", "D", "B")
+        cols = list(merged.columns)
+        self.assertEqual(cols,
+                         ["A id", "A gene", "A Exon(s)", "A-B ccode", "D id", "D gene", "D Exon(s)", "D-B ccode",
+                          "ref (B) id", "ref (B) gene"])
+        self.assertEqual(len(merged), 1, merged)
+
+
+    def test_sixway(self):
+        df = pd.DataFrame(columns=["A id", "A gene", "A Exon(s)", "A-D ccode", "B id", "B gene", "B Exon(s)", "B-D ccode",
+                     "ref (D) id", "ref (D) gene"])
+
+        df_two = pd.DataFrame(columns=["A id", "A gene", "A Exon(s)", "A-B ccode", "D id", "D gene", "D Exon(s)", "D-B ccode",
+                          "ref (B) id", "ref (B) gene"])
+
+        df_three = pd.DataFrame(columns=["B id", "B gene", "B Exon(s)", "B-A ccode", "D id", "D gene", "D Exon(s)", "D-A ccode",
+                          "ref (A) id", "ref (A) gene"])
+
+
+        df.loc[0] = ["as_1", "gidA_1", 2, "=", "bs_1", "gidB_1",  2, "=", "ds_1", "gidD_1"]
+        df.loc[1] = ["tidA_2", "gidA_2", 2, "-", "-", "gidD_2",  2, "-", "tidB_2", "refB_gid_2"]
+
+        df_two.loc[0] = ["as_1", "gidA_1", 2, "=", "ds_1", "gidD_1",  3, "=", "bs_1", "gidB_1"]
+        df_two.loc[1] = ["tidA_2", "gidA_2", 3, "-", "-", "-",  '-', "=", "tidD_2", "refD_gid_2"]
+
+        df_three.loc[0] = ["bs_1", "gidB_1", 2, "=", "ds_1", "gidD_1", 2, "=", "as_1", "as_1"]
+        df_three.loc[1] = ["tidB_2", "gidB_2", 3, "-", "-", "-", 3, "=", "tidA_2", "refA_gid_2"]
+
+        sixwayy = sixway(df, df_two, df_three, "A", "B", "D")
+        coln = list(sixwayy.columns)
+        self.assertEqual(coln, ["A", "B", "D", 'A Exon(s)', 'B Exon(s)', 'D Exon(s)', 'A-B ccode', 'A-D ccode',
+                                'B-A ccode', 'B-D ccode', 'D-A ccode', 'D-B ccode'])
+        self.assertEqual(len(sixwayy), 1, sixwayy)
+
+    def test_allmatch(self):
+
+        df = pd.DataFrame(columns=["A", "B", "D", 'A Exon(s)', 'B Exon(s)', 'D Exon(s)', 'A-B ccode', 'A-D ccode',
+                                'B-A ccode', 'B-D ccode', 'D-A ccode', 'D-B ccode'])
+        df.loc[0] = ["tidA_1", "tidB_1", "tidD_1", 2, 2, 2, "=","=","=","=","=","="]
+        df.loc[1] = ["tidA_1", "tidB_1", "tidD_1", 2, 3, 2, "=", "=", "=", "=", "=", "="]
+        df.loc[2] = ["tidA_1", "tidB_1", "tidD_1", 2, 2, 2, "=", "c", "=", "=", "j", "="]
+        df.loc[3] = ["tidA_1", "tidB_1", "tidD_1", 10, 2, 5, "=", "x", "=", "j", "=", "c"]
+
+        math = allmatches(df, "A", "B", "D")
 
 
 
